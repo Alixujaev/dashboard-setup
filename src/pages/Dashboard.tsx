450f4d2f4ee27b2d721b702a7fa314914238ea7d
Alixujaev <alixujaevislom@gmail.com>
2025-03-25 11:33:04 +0500

    fix

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,21 +1,30 @@
 const Dashboard = () => {
   async function sendZPLCommand() {
-    const device = await (navigator as any).usb.requestDevice({
-      filters: [{ vendor_id: 6495 }],
-    });
+    try {
+      const device = await (navigator as any).usb.requestDevice({
+        filters: [{ vendorId: 6495 }],
+      });
 
-    await device.open();
-    if (device.configuration === null) {
-      await device.selectConfiguration(1);
+      await device.open();
+      if (device.configuration === null) {
+        await device.selectConfiguration(1);
+      }
+
+      await device.claimInterface(
+        device.configuration.interfaces[0].interfaceNumber
+      );
+
+      const zplCommand = `^XA
+      ^FO50,50^ADN,36,20^FDHello, ZPL!^FS
+      ^XZ`;
+      const encoder = new TextEncoder();
+      const data = encoder.encode(zplCommand);
+
+      await device.transferOut(1, data);
+      console.log("ZPL Command sent successfully!");
+    } catch (error) {
+      console.error("Error sending ZPL Command:", error);
     }
-    await device.claimInterface(0);
-
-    const zplCommand = `^XA
-    ^FO50,50^ADN,36,20^FDHello, ZPL!^FS
-    ^XZ`;
-    const encoder = new TextEncoder();
-    const data = encoder.encode(zplCommand);
-    await device.transferOut(1, data);
   }
 
   return (
